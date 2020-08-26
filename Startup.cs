d077769617a5e66bd3d070a440e3d479a3afc84b
Cheah, Chris <chris.cheah@intel.com>
2020-08-26 10:40:12 +0800

    replaced SQL src to SQL Server Linux for container

--- Startup.cs
+++ Startup.cs
@@ -45,7 +45,10 @@ namespace ShoppingDenProductAPI
             #endregion
 
 
-            services.AddTransient<ICatalogRepository,CatalogRepository>(); 
+            services.AddTransient<ICatalogRepository, CatalogRepository>();
+            services.AddControllers().AddNewtonsoftJson(options =>
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
